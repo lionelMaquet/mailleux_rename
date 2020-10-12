39ccdfbe14ac4d16444533dfa70c0a07ab5f9db1
Lionel Maquet <Lionel.Maquet@meubles-mailleux.be>
2020-10-12 11:23:31 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace mailleux_rename
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mainPath = args[0];
+            string[] subdirectories = Directory.GetDirectories(mainPath);
+            string prepend = args[1];
+
+            foreach (string directory in subdirectories)
+            {
+                var dir = new DirectoryInfo(directory);
+                var dirName = dir.Name;
+                string[] files = Directory.GetFiles(directory);
+                Console.WriteLine(dirName);
+                foreach (string filename in files)
+                {
+
+                    var fileInfo = new FileInfo(filename);
+                    var filenameShort = fileInfo.Name;
+                    
+
+                    string oldFileName = filename;
+
+                    //picto
+                    if (filename.EndsWith("p.jpg"))
+                    {
+                        string newFileName = $"{directory}\\{prepend}-{dirName}-picto.jpg";
+                        System.IO.File.Move(oldFileName, newFileName);
+                    }
+
+                    else 
+                    {
+                        string newFileName = $"{directory}\\{prepend}-{dirName}-photo-0{filenameShort}";
+                        System.IO.File.Move(oldFileName, newFileName);
+                    }
+                }
+            }
+        }
+    }
+}
